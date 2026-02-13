d87250c3ae6e4c565e37b5555086d42ca1435588
Chidinma <chidinmaokeh75@gmail.com>
2026-02-13 20:00:58 +0100

    task 1

--- Control Flow/question.py
+++ Control Flow/question.py
@@ -1,12 +1,12 @@
 student_name = input("Enter your name: ")   
 grade = input("Enter your grade: ")
-if 3.5 <= float(grade) < 4.0:
+if 3.5 <= float(grade) and float(grade) <= 4.0:
     print("First Class Honours")
-elif 3.0 <= float(grade) < 3.5:
+elif 3.0 <= float(grade) and float(grade) <= 3.49:
     print("Second Class Honours (Upper Division)")
-elif 2.0 <= float(grade) < 3.0:
+elif 2.0 <= float(grade) and float(grade) <= 2.99:
     print("Second Class Honours (Lower Division)")
-elif 1.0 <= float(grade) < 2.0:
+elif 1.0 <= float(grade) and float(grade) <= 1.99:
     print("Third Class Honours")
 else:
     print(" Grade Not available")
