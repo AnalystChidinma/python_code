04a0de8bcd74dd746384ed56ae0f6bba97a01097
Chidinma <chidinmaokeh75@gmail.com>
2026-02-18 01:54:27 +0100

    added code documenation

--- Control Flow/task 2.py
+++ Control Flow/task 2.py
@@ -1,3 +1,8 @@
+"""
+Write a program that prints the numbers from 1 to 100. 
+But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+For numbers which are multiples of both three and five print “FizzBuzz”.
+"""
 numbers =range(1, 101)
 for number in numbers:
     if number % 3 == 0 and number % 5 == 0:
--- Control Flow/task1.py
+++ Control Flow/task1.py
@@ -1,3 +1,11 @@
+"""
+Write a program that takes a student's name and grade as input 
+and prints the corresponding honours classification based on the following criteria:
+- First Class Honours: 3.5 to 4.0
+- Second Class Honours (Upper Division): 3.0 to 3.49
+- Second Class Honours (Lower Division): 2.0 to 2.99
+"""
+
 student_name = input("Enter your name: ")   
 grade = input("Enter your grade: ")
 if 3.5 <= float(grade) and float(grade) <= 4.0:
